b2b0fce799309f5f84b30ede94e249172fd01ce3
aditya-dawadikar <mail.aditya.dawadikar@gmail.com>
2025-03-19 22:37:38 -0700

    Handle Cors

--- LogFilter/index.js
+++ LogFilter/index.js
@@ -1,12 +1,21 @@
 const express = require("express");
 const { router } = require('./routes/LogQueryRoutes');
 const { connectConsumer } = require('./controllers/LogConsumer');
+const cors = require("cors");
 
 const app = express();
 const PORT = 9001;
 
 // **Start Consumer**
 connectConsumer().catch(console.error);
+
+const corsConfig = {
+    "origin": "*",
+    "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+    "preflightContinue": false,
+    "optionsSuccessStatus": 204
+}
+app.use(cors(corsConfig))
 
 // **Add Router to Express app**
 app.use('/api/v1', router)
